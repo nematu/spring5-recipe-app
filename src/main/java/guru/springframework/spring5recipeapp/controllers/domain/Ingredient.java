150f3b8df4f100e1590b4c24d0a2730309d68bfa
nematu <77222135+nematu@users.noreply.github.com>
2021-10-28 21:51:44 -0700

    created Domain directory / established relationships

---
+++ src/main/java/guru/springframework/spring5recipeapp/controllers/domain/Ingredient.java
@@ -0,0 +1,89 @@
+package guru.springframework.spring5recipeapp.controllers.domain;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+/**
+ * Created by Nematu
+ */
+
+@Entity
+public class Ingredient
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+    private BigDecimal amount;
+
+    @OneToOne(fetch = FetchType.EAGER)
+    private UnitOfMeasure uom;
+
+    @ManyToOne
+    private Recipe recipe;
+
+    public Ingredient()
+    {
+
+    }
+
+    public Long getId()
+    {
+        return id;
+    }
+
+    public void setId(Long id)
+    {
+        this.id = id;
+    }
+
+    public String getDescription()
+    {
+        return description;
+    }
+
+    public void setDescription(String description)
+    {
+        this.description = description;
+    }
+
+    public BigDecimal getAmount()
+    {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount)
+    {
+        this.amount = amount;
+    }
+
+    public UnitOfMeasure getUam()
+    {
+        return uom;
+    }
+
+    public void setUam(UnitOfMeasure uam)
+    {
+        this.uom = uam;
+    }
+
+    public Recipe getRecipe()
+    {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe)
+    {
+        this.recipe = recipe;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Ingredient{" +
+                "id=" + id +
+                ", description='" + description + '\'' +
+                ", amount=" + amount +
+                '}';
+    }
+}
